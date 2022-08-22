f3c003f6dcb12120c063b98ebb2abea5684b0054
tomaszlemke <47919500+tomaszlemke@users.noreply.github.com>
2022-08-22 09:37:19 +0200

    changed logged in state to session - auto logout when browser or card closed

--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -19,6 +19,9 @@ export class AuthService {
     public router: Router,
     public ngZone: NgZone // NgZone service to remove outside scope warning
   ) {
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+
+
     /* Saving user data in localstorage when
     logged in and setting up null when logged out */
     this.afAuth.authState.subscribe(user => {
